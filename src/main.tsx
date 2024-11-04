7add048c693e840460cf849d581e50a221b958ed
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2024-11-04 14:05:17 +0900

    fix: コメントアウトの削除

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Canvas } from "@react-three/fiber";
-// import SampleThreeJS from "./SampleThreeJS";
 import SampleCamera from "./camera/SampleCamera";
 import SampleBulbLight from "./light/SampleBulbLight";
 import SampleHemiLight from "./light/SampleHemiLight";
@@ -10,24 +9,11 @@ import SampleFloorMesh from "./mesh/SampleFloorMesh";
 import SampleBallMesh from "./mesh/SampleBallMesh";
 import SampleBoxMesh from "./mesh/SampleBoxMesh";
 import { OrbitControls, Stats } from "@react-three/drei";
-// import { PresentationControls } from "@react-three/drei";
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
-        {/* <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <SampleThreeJS position={[-1.2, 0, 0]} />
-      <SampleThreeJS position={[1.2, 0, 0]} /> */}
         <SampleCamera />
         <SampleBulbLight />
         <SampleHemiLight />
